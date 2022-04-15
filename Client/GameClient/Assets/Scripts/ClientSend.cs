45696fb4a93d57d05510803fb8fdf69da7c077bf
KwonYiHyun <81015702+KwonYiHyun@users.noreply.github.com>
2022-04-15 16:40:18 +0900

    ClientSend to Server(UDP, TCP) Packet process

--- Client/GameClient/Assets/Scripts/ClientSend.cs
+++ Client/GameClient/Assets/Scripts/ClientSend.cs
@@ -7,7 +7,7 @@ public class ClientSend : MonoBehaviour
 {
     private static void SendTCPData(Packet _packet){
         _packet.WriteLength();
-        // ClientSendTCP-4 [최종 패킷 길이 int 4바이트 / 패킷번호 int 4바이트 / 내 클라이언트 id int 4바이트 / 닉네임 문자열 바이트배열]
+        // ClientSendTCP-4 [최종 패킷 길이 int 4바이트 / 패킷번호 int 4바이트 / 내 클라이언트 id int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열]
         Client.instance.tcp.SendData(_packet);
     }
 
@@ -24,7 +24,7 @@ public class ClientSend : MonoBehaviour
             _packet.Write(Client.instance.myId);
             // ClientSendTCP-2 [패킷번호 int 4바이트 / 내 클라이언트 id int 4바이트]
             _packet.Write(UIManager.instance.usernameField.text);
-            // ClientSendTCP-3 [패킷번호 int 4바이트 / 내 클라이언트 id int 4바이트 / 닉네임 문자열 바이트배열]
+            // ClientSendTCP-3 [패킷번호 int 4바이트 / 내 클라이언트 id int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열]
             SendTCPData(_packet);
         }
     }
