a50fa2e722e9543570a0ef018cbd22a6faba8df7
KwonYiHyun <81015702+KwonYiHyun@users.noreply.github.com>
2022-04-15 16:53:49 +0900

    ServerSend to Client(UDP, TCP) Packet process

--- Server/GameServer/GameServer/ServerSend.cs
+++ Server/GameServer/GameServer/ServerSend.cs
@@ -15,7 +15,7 @@ namespace GameServer
 
             // 앞의 함수들을 거쳐서 최종적으로 다음과같은 구조를 이룬다
             // 최종 문자열길이 int 4바이트 / 처음패킷생성할때 등록한 delegate번호 int 4바이트(패킷번호) / 순수문자열길이 int 4바이트 / 문자열 바이트배열 / 클라이언트번호 int 4바이트
-            // WelcomePacket-4 [최종 패킷길이 int 4바이트 / 패킷번호 int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열 / 보낼클라이언트 id int 4바이트]
+            // ServerSendTCP-4 [최종 패킷길이 int 4바이트 / 패킷번호 int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열 / 보낼클라이언트 id int 4바이트]
             // TCP는 서버연결용으로 사용하고 그 후는 UDP로 통신해서 구조가 이런듯하다
 
             // Client id에 패킷을 보낸다
@@ -27,7 +27,7 @@ namespace GameServer
             _packet.WriteLength();
             // 앞의 함수들을 거쳐서 최종적으로 다음과같은 구조를 이룬다
             // 최종 문자열길이 int 4바이트 / 패킷번호 int 4바이트 / 문자열 바이트배열
-            // UDPTest-3 [최종 패킷길이 int 4바이트 / 패킷번호 int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열]
+            // ServerSendUDP-3 [최종 패킷길이 int 4바이트 / 패킷번호 int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열]
             Server.clients[_toClient].udp.SendData(_packet);
         }
 
@@ -77,12 +77,12 @@ namespace GameServer
         #region Packets
         public static void Welcome(int _toClient, string _msg)
         {
-            // WelcomePacket-1 [패킷번호 int 4바이트]
+            // ServerSendTCP-1 [패킷번호 int 4바이트]
             using (Packet _packet = new Packet((int)ServerPackets.welcome))
             {
-                // WelcomePacket-2 [패킷번호 int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열]
+                // ServerSendTCP-2 [패킷번호 int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열]
                 _packet.Write(_msg);
-                // WelcomePacket-3 [패킷번호 int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열 / 보낼클라이언트 id int 4바이트]
+                // ServerSendTCP-3 [패킷번호 int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열 / 보낼클라이언트 id int 4바이트]
                 _packet.Write(_toClient);
 
                 // TCP는 서버연결용이기 때문에 구조가 복잡하다
@@ -92,10 +92,10 @@ namespace GameServer
 
         public static void UDPTest(int _toClient)
         {
-            // UDPTest-1 [패킷번호 int 4바이트]
+            // ServerSendUDP-1 [패킷번호 int 4바이트]
             using (Packet _packet = new Packet((int)ServerPackets.udpTest))
             {
-                // UDPTest-2 [패킷번호 int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열]
+                // ServerSendUDP-2 [패킷번호 int 4바이트 / 문자열길이 int 4바이트 / 문자열 바이트배열]
                 _packet.Write("A test packet for UDP");
 
                 SendUDPData(_toClient, _packet);
